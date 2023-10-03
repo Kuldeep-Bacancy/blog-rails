8d56964343691df1c0c5ad7a1a2607e01a56b0bd
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2023-10-03 10:52:55 +0530

    Remove slug callback

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,13 +2,9 @@ class Article < ApplicationRecord
 
   validates :title, presence: true
   validates :content, presence: true
+  validates :slug, presence: true
 
   has_one_attached :image
   belongs_to :user
 
-  before_save :create_slug_for_article
-
-  def create_slug_for_article
-    self.slug = title.gsub(' ', '-')
-  end
 end
