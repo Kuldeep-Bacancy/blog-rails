c37bff0b34404773091edf1356355f2feba47905
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2023-10-06 10:00:57 +0530

    normailize email

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+  normalizes :email, with: -> email { email.strip.downcase } # normalizes email to downcase after update or create
 
   has_many :black_list_tokens, dependent: :destroy
   has_many :articles, dependent: :destroy
