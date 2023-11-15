fb4554cdf4af7fc7dade048d936df9d495202e07
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2023-11-15 12:36:58 +0530

    Added specs for articles model

---
+++ spec/models/articles.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+
+RSpec.describe Article, type: :model do
+  describe 'associations' do
+    it { should have_one_attached(:image) }
+    it { should belong_to(:user) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:content) }
+    it { should validate_presence_of(:slug) }
+  end
+end
